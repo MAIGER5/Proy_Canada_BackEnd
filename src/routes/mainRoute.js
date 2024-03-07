09d733725ca739d6faa8af6872524d05f97e9103
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2024-03-07 15:55:46 -0500

    prubea final positiva del servidor

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -5,7 +5,7 @@ const mainRoute = Router()
 
 mainRoute.get("/", (req, res)=>{
     console.log(req.header);
-    res.send("ESTOY EN SERVIDOR ADECUADO")
+    res.send("esto es una prueba para que Kiki pueda ver")
   })
 // mainRouter.use('/product', productRoute)
 // mainRouter.use('/client', productClient)
