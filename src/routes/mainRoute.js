6fff11c6aaf7697c96cc0126575e6398d360077c
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2024-03-07 14:45:50 -0500

    cambiando una coma

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -4,7 +4,7 @@ const { Router } = require('express');
 const mainRoute = Router()
 
 mainRoute.get("/", (req, res)=>{
-    console,console.log(req.header);
+    console.log(req.header);
     res.send("ESTOY EN SERVIDOR ADECUADO")
   })
 // mainRouter.use('/product', productRoute)
