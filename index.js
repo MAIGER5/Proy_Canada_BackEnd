c63cce8e1af9d0399cf0c547b3ded88839106268
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2024-03-10 14:25:58 -0400

    Haciendo una prueba de servidr

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const PORT = 3000;
 
 
 
-server.listen(PORT, ()=>{
-    console.log(`Estoy escuchando en el puerto # ${PORT}`);
-    console.log(`Me encuentro en el servidor`)
-})
 conn.sync({ alter: true }).then(()=>{
+    server.listen(PORT, ()=>{
+        console.log(`Estoy escuchando en el puerto # ${PORT}`);
+        console.log(`Me encuentro en el servidor`)
+    })
 })
--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -5,7 +5,7 @@ const mainRoute = Router()
 
 mainRoute.get("/", (req, res)=>{
     console.log(req.header);
-    res.send("esto es una prueba para que Kiki pueda ver")
+    res.send("ELABORANDO UNA PRUEBA DE SERVIDOR")
   })
 // mainRouter.use('/product', productRoute)
 // mainRouter.use('/client', productClient)
