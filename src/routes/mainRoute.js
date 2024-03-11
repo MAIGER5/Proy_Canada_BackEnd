9f1e66dcbd8bd11536fa2a2ef95331c2cc0228ee
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2024-03-10 22:52:17 -0400

    new

--- src/routes/mainRoute.js
+++ src/routes/mainRoute.js
@@ -6,7 +6,11 @@ const mainRoute = Router()
 mainRoute.get("/", (req, res)=>{
     console.log(req.header);
     res.send("  QUISIERA LEVANTARTE A BESOIS")
-  })
+})
+mainRoute.get("/about", (req, res)=>{
+    console.log(req.header);
+    res.send("  VAMOS CON TODA KIKI")
+})
 // mainRouter.use('/product', productRoute)
 // mainRouter.use('/client', productClient)
 
