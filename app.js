666b060171239b64b601d2d54ea8e60461b47868
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2024-03-10 22:40:12 -0400

    otro

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const cors = require('cors');
-// const mainRoute = require('./src/routes/mainRoute.js');
+const mainRoute = require('./src/routes/mainRoute.js');
 
 // require('./db.js')
 
