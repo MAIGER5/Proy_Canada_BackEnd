50bbab6db1bb5263ac333a687da22acb056f4795
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2024-03-10 22:41:59 -0400

    nuevo toro

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const server = require('./app.js');
-// const { conn } = require('./db.js');
+const { conn } = require('./db.js');
 
 
 const PORT = 3000;
 
 
 
-server.listen(PORT, ()=>{
-    console.log(`Estoy escuchando en el puerto # ${PORT}`);
-    console.log(`Me encuentro en el servidor`)
+conn.sync({ alter: true }).then(()=>{
+    server.listen(PORT, ()=>{
+        console.log(`Estoy escuchando en el puerto # ${PORT}`);
+        console.log(`Me encuentro en el servidor`)
+    })
 })
-// conn.sync({ alter: false }).then(()=>{
-// })
