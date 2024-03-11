625ec93add063bcd69c0a76435c293f01ac6fdf9
MAIGER_OVALLE_RUZ <maiger5learning@gmail.com>
2024-03-10 22:13:07 -0400

    conectando la base de datos

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const { conn } = require('./db.js');
 
 
 const PORT = 3000;
--
 
 
-server.listen(PORT, ()=>{
-    console.log(`Estoy escuchando en el puerto # ${PORT}`);
-    console.log(`Me encuentro en el servidor`)
+
+conn.sync({ alter: true }).then(()=>{
+    server.listen(PORT, ()=>{
+        console.log(`Estoy escuchando en el puerto # ${PORT}`);
+        console.log(`Me encuentro en el servidor`)
+    })
 })
-// conn.sync({ alter: true }).then(()=>{
-// })
