a53fb740274664a3a1e852c066413c9e784d387c
maiger5 <maiger5learning@gmail.com>
2024-12-03 18:20:11 -0500

    correccion access-control-allow-origin: http:

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ const corsOptions = {
   app.use(cookieParser());
   app.use(morgan('dev'))
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', 'https://carmeusecolombia.maigerprofile.com'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
